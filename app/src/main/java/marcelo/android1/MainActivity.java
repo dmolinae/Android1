64068ed3b5ff237208bf90988f353d222daf696c
dmolinae <dmolinae@outlook.com>
2016-03-29 14:13:52 -0300

    Initial commit

---
+++ app/src/main/java/marcelo/android1/MainActivity.java
@@ -0,0 +1,125 @@
+package marcelo.android1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    int i = -1;
+    int total = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView err = (TextView)findViewById(R.id.tvTexto);
+        err.setText("INICIO");
+        Button btnSig = (Button)findViewById(R.id.btSiguiente);
+        Button btnAnt = (Button)findViewById(R.id.btAnterior);
+        btnAnt.setOnClickListener(this);
+        btnSig.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        final TextView err = (TextView)findViewById(R.id.tvTexto);
+        int id = v.getId();
+        if (id == R.id.btSiguiente) {
+            i++;
+            Thread nt = new Thread() {
+                @Override
+                public void run() {
+                    final String datos = leer();
+                    runOnUiThread(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        listaEntrada(obtDatosJSON(datos));
+                                    }catch (Exception e){
+                                        err.setText("No existen más mensajes");
+                                        i--;
+                                    }
+                                }
+                            });
+                }
+            };
+            nt.start();
+        }
+        if (id == R.id.btAnterior) {
+            i--;
+            if(i == -1) i = 0;
+            Thread nt = new Thread() {
+                @Override
+                public void run() {
+                    final String datos = leer();
+                    runOnUiThread(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    listaEntrada(obtDatosJSON(datos));
+                                }
+                            });
+                }
+            };
+            nt.start();
+        }
+    }
+
+    public void listaEntrada(ArrayList<String> datos) {
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+        JSONArray json = new JSONArray(datos);
+        TextView err = (TextView)findViewById(R.id.tvTexto);
+        int ultimoMsg = datos.size();
+        err.setText(datos.get(i));
+    }
+
+    public String leer() {
+        HttpClient cliente = new DefaultHttpClient();
+        HttpContext contexto = new BasicHttpContext();
+        HttpGet httpget = new HttpGet("http://bafuach.netau.net/android/GetData.php");
+        String resultado = null;
+        try{
+            HttpResponse response = cliente.execute(httpget, contexto);
+            HttpEntity entity = response.getEntity();
+            resultado = EntityUtils.toString(entity, "UTF-8");
+        }catch(Exception e){
+            //TODO: handle exception
+        }
+        return resultado;
+    }
+
+    public ArrayList<String> obtDatosJSON(String datos) {
+        TextView err = (TextView)findViewById(R.id.tvTexto);
+        ArrayList<String> listado = new ArrayList<String>();
+        try{
+            JSONArray json = new JSONArray(datos);
+            String texto = "";
+            for (int i = 0; i < json.length(); i++){
+                texto = "id: "+json.getJSONObject(i).getString("id") + "\n" +
+                        "Titulo: "+json.getJSONObject(i).getString("titulo") + "\n" +
+                        "Mensaje: "+json.getJSONObject(i).getString("mensaje");
+                listado.add(texto);
+            }
+        }catch (Exception e){
+            //TODO:handle Exception
+        }
+        return listado;
+    }
+}
---
+++ app/src/main/java/marcelo/android1/Principal.java
@@ -0,0 +1,67 @@
+package marcelo.android1;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by Marcelo on 10/03/2016.
+ */
+public class Principal extends Activity{
+
+    String user;
+    TextView txt_user, logoff;
+    ImageButton anuncio;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal);
+
+        txt_user = (TextView) findViewById(R.id.textView_nombre);
+        logoff = (TextView) findViewById(R.id.textView_logoff);
+        anuncio = (ImageButton) findViewById(R.id.bt_anuncio);
+
+        Bundle extras = getIntent().getExtras();
+
+        //Obtenemos datos enviados desde el Login (usuario);
+        if(extras != null){
+            user = extras.getString("user");
+        }else{
+            user = "error";
+        }
+
+        txt_user.setText("Bienvenido " + user);//Cmbiamos el texto al nombre del registrado
+
+        logoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cerrar sesion y regresa a la ventana anterior
+                finish();
+
+            }
+        });
+
+        anuncio.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(Principal.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0){
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
